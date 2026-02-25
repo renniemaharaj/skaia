6eff293822f882526641c43bb53ea62ee4c3446c
renniemaharaj <rvesprey@gmail.com>
2026-02-25 01:05:02 -0400

    unlock cors

--- backend/main.go
+++ backend/main.go
@@ -64,9 +64,9 @@ func main() {
 	r.Use(cors.Handler(cors.Options{
 		AllowedOrigins:   []string{"*"},
 		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
-		ExposedHeaders:   []string{"Link"},
-		AllowCredentials: true,
+		AllowedHeaders:   []string{"*"},
+		ExposedHeaders:   []string{"*"},
+		AllowCredentials: false,
 		MaxAge:           300,
 	}))
 
